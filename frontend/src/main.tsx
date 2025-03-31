f493ae1580df84c8986fe30b9d300923bc36f354
Srikar Yalam <srikar.yalam@gmail.com>
2025-03-31 00:33:47 -0500

    configured backend to work with the charts

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,11 +1,11 @@
-import './App.css'
+import './App.css';
 
-import { createRoot } from 'react-dom/client'
+import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Route, Routes } from "react-router";
-
 import Layout from './components/Layout.tsx';
-import Menu from './pages/Menu.tsx'
+import Menu from './pages/Menu.tsx';
 import SalesReport from './pages/SalesReport.tsx';
+import ChartPage from './pages/ChartPage'; // Import ChartPage
 
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
@@ -13,6 +13,7 @@ createRoot(document.getElementById("root")!).render(
       <Route element={<Layout />}>
         <Route index element={<Menu />} />
         <Route path='/salesreport' element={<SalesReport />} />
+        <Route path='/charts' element={<ChartPage />} /> {}
       </Route>
     </Routes>
   </BrowserRouter>
