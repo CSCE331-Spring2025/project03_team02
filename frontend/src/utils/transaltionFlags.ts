8d8c53e165899aea20705195eba05e94d03f5a5a
rodrigoorozco <rodrigoorozco025@gmail.com>
2025-04-10 18:22:49 -0500

    Limited Country Flags Supported

---
+++ frontend/src/utils/transaltionFlags.ts
@@ -0,0 +1,40 @@
+export const translationFlags: Record<string, string> = {
+    // common languages
+    en: "us",     // English - United States
+    es: "mx",     // Spanish - Mexico
+    fr: "fr",     // French - France
+    de: "de",     // German - Germany
+    ja: "jp",     // Japanese - Japan
+    zh: "cn",     // Chinese - China
+    ko: "kr",     // Korean - South Korea
+    ar: "sa",     // Arabic - Saudi Arabia
+    hi: "in",     // Hindi - India
+    ru: "ru",     // Russian - Russia
+    pt: "pt",     // Portuguese - Portugal
+    it: "it",     // Italian - Italy
+    nl: "nl",     // Dutch - Netherlands
+  
+    // additional languages spoken in Texas and U.S.
+    vi: "vn",     // Vietnamese - Vietnam
+    ta: "lk",     // Tamil - Sri Lanka 
+    ur: "pk",     // Urdu - Pakistan
+    fa: "ir",     // Farsi - Iran
+    pl: "pl",     // Polish - Poland
+    tr: "tr",     // Turkish - Turkey
+    el: "gr",     // Greek - Greece
+    he: "il",     // Hebrew - Israel
+    am: "et",     // Amharic - Ethiopia
+    ha: "ng",     // Hausa - Nigeria
+    th: "th",     // Thai - Thailand
+    gu: "in",     // Gujarati - India
+    pa: "in",     // Punjabi - India
+    bn: "bd",     // Bengali - Bangladesh
+    ro: "ro",     // Romanian - Romania
+    uk: "ua",     // Ukrainian - Ukraine
+    sv: "se",     // Swedish - Sweden
+    tl: "ph",     // Tagalog - Philippines
+    cmn: "tw",    // Mandarin Chinese - Taiwan
+    yue: "hk",    // Cantonese - Hong Kong
+    //... more to come
+  };
+  
