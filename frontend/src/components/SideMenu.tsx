fe41c38f62c42be76e36d460bac505edb0065466
Sumit Nalavade <vs.nalavade2003@gmail.com>
2025-04-13 18:24:15 -0500

    Add kitchen icon to side menu

--- frontend/src/components/SideMenu.tsx
+++ frontend/src/components/SideMenu.tsx
@@ -5,9 +5,10 @@ import { RiDrinksLine } from "react-icons/ri";
 import { LuWarehouse } from "react-icons/lu";
 import { TbBrandGoogleAnalytics } from "react-icons/tb";
 import { BsPeople } from "react-icons/bs";
+import { TbToolsKitchen2 } from "react-icons/tb";
 
 interface SideMenuProps {
-    currentPage: "menu" | "inventory" | "reports" | "employees"
+    currentPage: "menu" | "inventory" | "reports" | "employees" | "kitchen"
 }
 const SideMenu: React.FC<SideMenuProps> = ({ currentPage }) => {
     const location = useLocation();
@@ -16,7 +17,7 @@ const SideMenu: React.FC<SideMenuProps> = ({ currentPage }) => {
         path.includes("inventory") ? "inventory" :
             path.includes("reports") ? "reports" :
                 path.includes("employees") ? "employees" :
-                    "menu"
+                    path.includes("kitchen") ? "kitchen" : "menu"
     );
 
     return (
@@ -24,6 +25,10 @@ const SideMenu: React.FC<SideMenuProps> = ({ currentPage }) => {
             <Link to="/" className={`flex flex-col items-center w-full p-2 hover:bg-gray-100 cursor-pointer ${activePage !== 'menu' ? "text-gray-400 " : ""}`}>
                 <RiDrinksLine className="text-2xl" />
                 <p>Menu</p>
+            </Link>
+            <Link to="/kitchen" className={`flex flex-col items-center w-full p-2 hover:bg-gray-100 cursor-pointer ${activePage !== 'kitchen' ? "text-gray-400 " : ""}`}>
+                <TbToolsKitchen2 className="text-2xl" />
+                <p>Kitchen</p>
             </Link>
             <Link to="/inventory" className={`flex flex-col items-center w-full p-2 hover:bg-gray-100 cursor-pointer ${activePage !== 'inventory' ? "text-gray-400" : ""}`}>
                 <LuWarehouse className="text-2xl" />
